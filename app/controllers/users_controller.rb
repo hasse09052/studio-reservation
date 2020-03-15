ad6556d8099501e09104f3eb4b92db1dbda5ad4b
Kazuki Hasegawa <hasse09052@gmail.com>
2020-03-15 17:33:36 +0900

    余分な空白を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
-
   def index
     @users = User.all
   end
@@ -60,7 +59,5 @@ class UsersController < ApplicationController
         redirect_to(root_url)
       end
     end
-
-  
 
 end
