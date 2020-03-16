bcdb9caa26f1df9f358d6823f8c65791af1289f7
Kazuki Hasegawa <hasse09052@gmail.com>
2020-03-16 11:55:57 +0900

    予約詳細｜予約一覧に戻るボタン追加、マイリスト後一覧にリダイレクトするよう変更

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -3,15 +3,26 @@ class MylistsController < ApplicationController
   def create
     @reservation = Reservation.find(params[:reservation_id])
     @mylist = Mylist.new(user_id: current_user.id, reservation_id: params[:reservation_id])
-    @mylist.save
-    redirect_to @reservation
+    if @mylist.save
+      flash[:success] = "マイリストに追加しました"
+      redirect_to "/reservations/table/1"
+    else
+      flash[:danger] = "マイリストの追加に失敗しました"
+      redirect_to @reservation
+    end
   end
 
   def destroy
     @reservation = Reservation.find(params[:reservation_id])
     @mylist = Mylist.find_by(user_id: current_user.id, reservation_id: params[:reservation_id])
     @mylist.destroy
-    redirect_to @reservation
+    if @mylist.destroy
+      flash[:success] = "マイリストから解除しました"
+      redirect_to "/reservations/table/1"
+    else
+      flash[:danger] = "マイリストの解除に失敗しました"
+      redirect_to @reservation
+    end
   end
 
 end
