3c4b7ad7ef1589b1c572e90882c85a8d0c8503da
Kazuki Hasegawa <hasse09052@gmail.com>
2020-03-23 14:07:32 +0900

    ヘッダー｜SP版ハンバーガーメニューへ変更

--- app/javascript/packs/libs.js
+++ app/javascript/packs/libs.js
@@ -1,30 +1,48 @@
-$(function() {
+$(function () {
+  // ハンバーガーメニュー開閉
+  $("#hamburgerBtn").click(() => {
+    const menus = $("#headerMenus");
+
+    if (menus.hasClass("-open")) {
+      $("#hamburgerBtn").toggleClass("-open");
+      menus.toggleClass("-open");
+      $("#hamburgerBtnText").text("Menu");
+      menus.stop(1, 1).slideUp();
+    }
+    else {
+      $("#hamburgerBtn").toggleClass("-open");
+      menus.toggleClass("-open");
+      $("#hamburgerBtnText").text("Close");
+      menus.stop(1, 1).slideDown();
+    }
+  });
+
   // モーダル非表示の定義
   const hiddenDeleteModal = () => {
     $("#overlay").fadeOut("fast");
     $("#deleteModal").fadeOut("fast");
     $("#logoutModal").fadeOut("fast");
-  }
+  };
 
   // キャンセルをクリックでモーダル非表示
-  $("#overlayHiddenBtn").click(()=>{
+  $("#overlayHiddenBtn").click(() => {
     hiddenDeleteModal();
-  })
+  });
 
   // オーバーレイをクリックでモーダル非表示
-  $("#overlay").click(()=>{
+  $("#overlay").click(() => {
     hiddenDeleteModal();
-  })
+  });
 
   // ログアウトのモーダル表示
-  $("#logoutBtn").click(()=>{
+  $("#logoutBtn").click(() => {
     $("#overlay").fadeIn("fast");
     $("#logoutModal").fadeIn("fast");
-  })
+  });
 
   // 予約削除のモーダル表示
-  $("#deleteBtn").click(()=>{
+  $("#deleteBtn").click(() => {
     $("#overlay").fadeIn("fast");
     $("#deleteModal").fadeIn("fast");
-  })
+  });
 });
