923f7aa7f0b7c6bd685349674977344c3fc81517
Kazuki Hasegawa <hasse09052@gmail.com>
2020-03-28 03:30:55 +0900

    SP版でcssのhover無効に設定

--- app/javascript/packs/libs.js
+++ app/javascript/packs/libs.js
@@ -1,3 +1,24 @@
+// SP版でcssのhoverが付くのを無効
+var touch = 'ontouchstart' in document.documentElement
+            || navigator.maxTouchPoints > 0
+            || navigator.msMaxTouchPoints > 0;
+if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+ 
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+ 
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
+
 $(function () {
   // ハンバーガーメニュー開閉
   $("#hamburgerBtn").click(() => {
